03ce73bbda513231cd3ea887048e34338e628d22
Trish <trishajjohnson@gmail.com>
2022-01-04 11:59:40 -0800

    graph exercises

---
+++ graph.js
@@ -0,0 +1,85 @@
+class Node {
+  constructor(value, adjacent = new Set()) {
+    this.value = value;
+    this.adjacent = adjacent;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.nodes = new Set();
+  }
+
+  // this function accepts a Node instance and adds it to the nodes property on the graph
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
+
+  // this function accepts an array of Node instances and adds them to the nodes property on the graph
+  addVertices(vertexArray) {
+    for(let v of vertexArray) {
+      this.addVertex(v);
+    }
+  }
+
+  // this function accepts two vertices and updates their adjacent values to include the other vertex
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
+
+  // this function accepts two vertices and updates their adjacent values to remove the other vertex
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
+
+  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
+  removeVertex(vertex) {
+    this.nodes.delete(vertex);
+  }
+
+  // this function returns an array of Node values using DFS
+  depthFirstSearch(start) {
+    let toVisitStack = [start];
+    let result = []
+    let visited = new Set(start.value);
+
+    while(toVisitStack.length > 0) {
+      let currentP = toVisitStack.pop();
+      result.push(currentP.value);
+
+      for(let adj of currentP.adjacent) {
+        if(!visited.has(adj.value)) {
+          toVisitStack.push(adj);
+          visited.add(adj.value);
+        }
+      }
+    }
+    return result;
+  }
+
+  // this function returns an array of Node values using BFS
+  breadthFirstSearch(start) {
+    let toVisitQueue = [start];
+    let result = [];
+    let visited = new Set(start.value);
+
+    while(toVisitQueue.length > 0) {
+      let currentP = toVisitQueue.shift();
+      result.push(currentP.value);
+
+      for(let adj of currentP.adjacent) {
+        if(!visited.has(adj.value)) {
+          toVisitQueue.push(adj);
+          visited.add(adj.value);
+        }
+      }
+    }
+
+    return result;
+    
+  }
+}
+
+module.exports = {Graph, Node}
